45efb84d7d5b1eceae495657df53fec9aa80fe22
Luca Landry <lucalandry@icloud.com>
2020-10-29 13:28:46 +0100

    [F.T] routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BlueComponent } from './blue/blue.component';
+import { RedComponent } from './red/red.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+      {path: 'red', component: RedComponent},
+      {path: 'blue', component: BlueComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/grey/grey.component.ts
+++ src/app/grey/grey.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-grey',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GreyComponent implements OnInit {
 
-  ids: Array<number> = [1, 2, 3, 4]
+  ids: Array<number> = []
 
   constructor() { }
 
