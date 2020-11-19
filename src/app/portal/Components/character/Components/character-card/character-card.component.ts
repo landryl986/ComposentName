c99a65d63b2b4dbbe11431d8224ba764f05882c1
Luca Landry <lucalandry@icloud.com>
2020-11-19 16:22:10 +0100

    not finished card getall characters

---
+++ src/app/portal/Components/character/Components/character-card/character-card.component.ts
@@ -0,0 +1,23 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ICharacter} from '../../Interfaces/ICharacter';
+
+@Component({
+  selector: 'app-character-card',
+  templateUrl: './character-card.component.html',
+  styleUrls: ['./character-card.component.css']
+})
+export class CharacterCardComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() character: ICharacter;
+  @Output() characterSelected = new EventEmitter<ICharacter>();
+
+  ngOnInit(): void {
+  }
+
+  selectCharacter(): void {
+    this.characterSelected.emit(this.character);
+  }
+
+}
